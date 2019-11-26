8e02fdf5cd704adca823121e01dcf5cd78c5993b
sashaadazhii <sashki.sashka@gmail.com>
2019-11-26 12:43:25 +0200

    header sticky+dot mobile

---
+++ exam/js/main.js
@@ -0,0 +1,15 @@
+let secInMin = 60;
+let minInHour = 60;
+let year = 3;
+let dayInYear = 365;
+let tys = year * dayInYear * secInMin * minInHour;
+
+console.log(tys);
+// let answer = prompt('How much is the fish?');
+
+
+let question1 = parseInt(prompt('number 1'));
+let question2 = parseInt(prompt('number 2'));
+
+let answer = question1 + question2;
+alert(answer);
