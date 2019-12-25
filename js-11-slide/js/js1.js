399e94c3ccae323c088316ac1f84520a16289f00
sashaadazhii <sashki.sashka@gmail.com>
2019-12-25 18:26:37 +0200

    js-slider

---
+++ js-11-slide/js/js1.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    // $(document).ready(function () {
+    //     $('.lider-container').slick({
+    //         lazyLoad: 'ondemand',
+    //         slidesToShow: 2,
+    //         slidesToScroll: 1
+    //     });
+    // });
+})
